1a0ce9b9ce10a10147951b3afffb12b3380a157d
gotree94 <49811923+gotree94@users.noreply.github.com>
2022-11-09 11:50:06 +0900

    init commit

---
+++ ExamMySQL/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace ExamMySQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDate = new System.Windows.Forms.TextBox();
+            this.textBoxBarcode = new System.Windows.Forms.TextBox();
+            this.listViewPhoneBook = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxWorker = new System.Windows.Forms.TextBox();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonCount = new System.Windows.Forms.Button();
+            this.Num_wash = new System.Windows.Forms.Label();
+            this.button_Export = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(54, 20);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(109, 42);
+            this.buttonInsert.TabIndex = 2;
+            this.buttonInsert.Text = "추가하기";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(21, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "날  짜";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(21, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "바코드";
+            // 
+            // textBoxDate
+            // 
+            this.textBoxDate.Location = new System.Drawing.Point(79, 22);
+            this.textBoxDate.Name = "textBoxDate";
+            this.textBoxDate.Size = new System.Drawing.Size(148, 21);
+            this.textBoxDate.TabIndex = 0;
+            // 
+            // textBoxBarcode
+            // 
+            this.textBoxBarcode.Location = new System.Drawing.Point(79, 78);
+            this.textBoxBarcode.Name = "textBoxBarcode";
+            this.textBoxBarcode.Size = new System.Drawing.Size(148, 21);
+            this.textBoxBarcode.TabIndex = 1;
+            // 
+            // listViewPhoneBook
+            // 
+            this.listViewPhoneBook.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listViewPhoneBook.FullRowSelect = true;
+            this.listViewPhoneBook.HideSelection = false;
+            this.listViewPhoneBook.Location = new System.Drawing.Point(13, 78);
+            this.listViewPhoneBook.Name = "listViewPhoneBook";
+            this.listViewPhoneBook.Size = new System.Drawing.Size(663, 204);
+            this.listViewPhoneBook.TabIndex = 7;
+            this.listViewPhoneBook.UseCompatibleStateImageBehavior = false;
+            this.listViewPhoneBook.View = System.Windows.Forms.View.Details;
+            this.listViewPhoneBook.SelectedIndexChanged += new System.EventHandler(this.listViewPhoneBook_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "날짜";
+            this.columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "바코드 번호";
+            this.columnHeader3.Width = 250;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "작업자";
+            this.columnHeader4.Width = 200;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(380, 20);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.buttonDelete.Size = new System.Drawing.Size(109, 42);
+            this.buttonDelete.TabIndex = 4;
+            this.buttonDelete.Text = "삭제하기";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Location = new System.Drawing.Point(534, 20);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(109, 42);
+            this.buttonSelect.TabIndex = 5;
+            this.buttonSelect.Text = "조회하기";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxWorker);
+            this.groupBox1.Controls.Add(this.textBoxBarcode);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxDate);
+            this.groupBox1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 110);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "정보";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(21, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "작업자";
+            // 
+            // textBoxWorker
+            // 
+            this.textBoxWorker.Location = new System.Drawing.Point(79, 51);
+            this.textBoxWorker.Name = "textBoxWorker";
+            this.textBoxWorker.Size = new System.Drawing.Size(148, 21);
+            this.textBoxWorker.TabIndex = 1;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(219, 20);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(109, 42);
+            this.buttonUpdate.TabIndex = 3;
+            this.buttonUpdate.Text = "수정하기";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.listViewPhoneBook);
+            this.groupBox2.Controls.Add(this.buttonInsert);
+            this.groupBox2.Controls.Add(this.buttonDelete);
+            this.groupBox2.Controls.Add(this.buttonUpdate);
+            this.groupBox2.Controls.Add(this.buttonSelect);
+            this.groupBox2.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 147);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(695, 301);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "조회 및 제어";
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonReset.Location = new System.Drawing.Point(285, 12);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(111, 54);
+            this.buttonReset.TabIndex = 6;
+            this.buttonReset.Text = "리셋";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // buttonCount
+            // 
+            this.buttonCount.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonCount.Location = new System.Drawing.Point(285, 72);
+            this.buttonCount.Name = "buttonCount";
+            this.buttonCount.Size = new System.Drawing.Size(111, 56);
+            this.buttonCount.TabIndex = 6;
+            this.buttonCount.Text = "세탁회수 카운트";
+            this.buttonCount.UseVisualStyleBackColor = true;
+            this.buttonCount.Click += new System.EventHandler(this.buttonCount_Click);
+            // 
+            // Num_wash
+            // 
+            this.Num_wash.AutoSize = true;
+            this.Num_wash.BackColor = System.Drawing.Color.Transparent;
+            this.Num_wash.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Num_wash.ForeColor = System.Drawing.Color.Black;
+            this.Num_wash.Location = new System.Drawing.Point(587, 66);
+            this.Num_wash.Name = "Num_wash";
+            this.Num_wash.Size = new System.Drawing.Size(68, 75);
+            this.Num_wash.TabIndex = 7;
+            this.Num_wash.Text = "0";
+            this.Num_wash.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button_Export
+            // 
+            this.button_Export.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Export.Location = new System.Drawing.Point(25, 469);
+            this.button_Export.Name = "button_Export";
+            this.button_Export.Size = new System.Drawing.Size(121, 35);
+            this.button_Export.TabIndex = 8;
+            this.button_Export.Text = "EXCEL Export";
+            this.button_Export.UseVisualStyleBackColor = true;
+            this.button_Export.Click += new System.EventHandler(this.button_Export_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(719, 516);
+            this.Controls.Add(this.button_Export);
+            this.Controls.Add(this.Num_wash);
+            this.Controls.Add(this.buttonCount);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "BUS Barcode Manager  Ver 0.9";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDate;
+        private System.Windows.Forms.TextBox textBoxBarcode;
+        private System.Windows.Forms.ListView listViewPhoneBook;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonSelect;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxWorker;
+        private System.Windows.Forms.Button buttonCount;
+        private System.Windows.Forms.Label Num_wash;
+        private System.Windows.Forms.Button button_Export;
+    }
+}
+
---
+++ ExamMySQL/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ExamMySQL
+{
+    public partial class Form1 : Form
+    {
+        string _server = "localhost";
+        int _port = 3308;
+        string _database = "new_schema";
+        string _id = "root";
+        string _pw = "root";
+        string _connectionAddress = "";
+        int index_sel = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _connectionAddress = string.Format("Server={0};Port={1};Database={2};Uid={3};Pwd={4}", _server, _port, _database, _id, _pw);
+            selectTable();
+
+            textBoxDate.Text = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+            KeyPreview = true;
+
+            // 리스트를 가장 최신값으로 보여주기
+            // 아래의 2가지 방법중에서 2번 방법이 동작됨.
+            // 1.
+            //listViewPhoneBook.EnsureVisible(listViewPhoneBook.Items.Count - 1);
+            // 2.
+            listViewPhoneBook.TopItem = listViewPhoneBook.Items[listViewPhoneBook.Items.Count - 1];
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
+                {
+                    textBoxDate.Text = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+
+                    mysql.Open();
+                    string insertQuery = string.Format("INSERT INTO accounts_table (id, date, barcode, name) VALUES ('{0}', '{1}', '{2}', '{3}');", index_sel+1, textBoxDate.Text, textBoxBarcode.Text, textBoxWorker.Text);
+
+                    MySqlCommand command = new MySqlCommand(insertQuery, mysql);
+                    if (command.ExecuteNonQuery() != 1)
+                        MessageBox.Show("Failed to insert data.");
+
+                    textBoxDate.Text = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+                    textBoxBarcode.Text = "";
+                    //textBoxWorker.Text = "";
+
+                    selectTable();
+
+                    listViewPhoneBook.TopItem = listViewPhoneBook.Items[listViewPhoneBook.Items.Count - 1];
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
+                {
+                    textBoxDate.Text = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+
+                    mysql.Open();
+                    int pos = listViewPhoneBook.SelectedItems[0].Index;
+                    int index = Convert.ToInt32(listViewPhoneBook.Items[pos].Text);
+                    string updateQuery = string.Format("UPDATE accounts_table SET date = '{1}', barcode = '{2}', name = '{3}' WHERE id={0};", index, textBoxDate.Text, textBoxBarcode.Text, textBoxWorker.Text);
+
+                    MySqlCommand command = new MySqlCommand(updateQuery, mysql);
+                    if (command.ExecuteNonQuery() != 1)
+                        MessageBox.Show("Failed to delete data.");
+
+                    textBoxDate.Text = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+                    textBoxBarcode.Text = "";
+                    //textBoxWorker.Text = "";
+
+                    selectTable();
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
+                {
+                    mysql.Open();
+                    int pos = listViewPhoneBook.SelectedItems[0].Index;
+                    int index = Convert.ToInt32(listViewPhoneBook.Items[pos].Text);
+                    string deleteQuery = string.Format("DELETE FROM accounts_table WHERE id={0};", index);
+
+                    MySqlCommand command = new MySqlCommand(deleteQuery, mysql);
+                    if (command.ExecuteNonQuery() != 1)
+                        MessageBox.Show("Failed to delete data.");
+
+                    textBoxDate.Text = "";
+                    textBoxBarcode.Text = "";
+                    //textBoxWorker.Text = "";
+
+                    selectTable();
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            selectTable();
+        }
+
+        private void selectTable()
+        {
+            try
+            {
+                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
+                {
+                    mysql.Open();
+                    string selectQuery = string.Format("SELECT * FROM accounts_table");
+
+                    MySqlCommand command = new MySqlCommand(selectQuery, mysql);
+                    MySqlDataReader table = command.ExecuteReader();
+
+                    listViewPhoneBook.Items.Clear();
+
+                    while (table.Read())
+                    {
+                        ListViewItem item = new ListViewItem();
+                        item.Text = table["id"].ToString();
+                        item.SubItems.Add(table["date"].ToString());
+                        item.SubItems.Add(table["barcode"].ToString());
+                        item.SubItems.Add(table["name"].ToString());
+
+                        listViewPhoneBook.Items.Add(item);
+                    }
+
+                    table.Close();
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void listViewPhoneBook_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListView listview = sender as ListView;
+
+            int index = listview.FocusedItem.Index;
+            index_sel = index;
+            textBoxDate.Text = listview.Items[index].SubItems[1].Text;
+            textBoxBarcode.Text = listview.Items[index].SubItems[2].Text;
+            textBoxWorker.Text = listview.Items[index].SubItems[3].Text;
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            textBoxDate.Text = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+            textBoxBarcode.Text = "";
+            //textBoxWorker.Text = "";
+            Num_wash.Text = "0";
+        }
+
+        private void buttonCount_Click(object sender, EventArgs e)
+        {
+            textBoxDate.Text = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+
+            try
+            {
+                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
+                {
+                    mysql.Open();
+                    string selectQuery = string.Format("SELECT COUNT(*) FROM accounts_table WHERE barcode = '{0}';", textBoxBarcode.Text);
+                    //string selectQuery = string.Format("SELECT COUNT(*) FROM accounts_table WHERE barcode = 'BINX71A8239';");
+
+                    MySqlCommand command = new MySqlCommand(selectQuery, mysql);
+                    int totalCount = Convert.ToInt32(command.ExecuteScalar());
+
+                    Num_wash.Text = Convert.ToString(totalCount);
+
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+        private void button_Export_Click(object sender, EventArgs e)
+        {
+            // Getting the location and file name of the excel to save from user
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.CheckPathExists = true;
+            saveFileDialog.AddExtension = true;
+            saveFileDialog.ValidateNames = true;
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            saveFileDialog.DefaultExt = ".xls";
+            saveFileDialog.Filter = "Microsoft Excel Workbook (*.xls)|*.xlsx";
+            saveFileDialog.FileName = "StudentData".ToString();
+        }
+
+        //protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        //{
+        //    Keys key = keyData & ~(Keys.Shift | Keys.Control);
+
+        //    switch (key)
+        //    {
+        //        //case Keys.F10:// 조합키 사용 시
+        //        //    if ((keyData & Keys.Control) != 0)
+        //        //    {
+        //        //        MessageBox.Show("Ctrl+F");
+        //        //    }
+        //        //    break;
+        //        case Keys.F1:
+        //            MessageBox.Show("A");
+        //            break;
+        //        case Keys.F2:
+        //            MessageBox.Show("S");
+        //            break;
+        //        case Keys.F3:
+        //            MessageBox.Show("D");
+        //            break;
+        //        case Keys.F4:
+        //            selectTable();
+        //            break;
+        //        case Keys.F5:
+        //            textBoxDate.Text = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+        //            textBoxBarcode.Text = "";
+        //            //textBoxWorker.Text = "";
+        //            Num_wash.Text = "0";
+        //            break;
+        //        case Keys.F6:
+        //            textBoxDate.Text = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+        //            try
+        //            {
+        //                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
+        //                {
+        //                    mysql.Open();
+        //                    string selectQuery = string.Format("SELECT COUNT(*) FROM accounts_table WHERE barcode = '{0}';", textBoxBarcode.Text);
+        //                    //string selectQuery = string.Format("SELECT COUNT(*) FROM accounts_table WHERE barcode = 'BINX71A8239';");
+
+        //                    MySqlCommand command = new MySqlCommand(selectQuery, mysql);
+        //                    int totalCount = Convert.ToInt32(command.ExecuteScalar());
+
+        //                    Num_wash.Text = Convert.ToString(totalCount);
+
+        //                }
+        //            }
+        //            catch (Exception exc)
+        //            {
+        //                MessageBox.Show(exc.Message);
+        //            }
+        //            break;
+        //        default:
+        //            //MessageBox.Show("지정되지 않은 키입니다.");
+        //            //return base.ProcessCmdKey(ref msg, keyData);
+        //            break;
+        //    }
+        //    return true;
+        //}
+    }
+}
